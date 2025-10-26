3baa7bbcda60912b1852f552a0fa2df33e30b57a
carb222 <44617896+carb222@users.noreply.github.com>
2025-10-26 13:39:55 +0000

    Update Coffee_Dashboard.py

--- Coffee_Dashboard.py
+++ Coffee_Dashboard.py
@@ -13,7 +13,7 @@ st.set_page_config(
 alt.themes.enable("dark")
 
 #Load Data
-df_reshaped = pd.read_csv('index_1.csv')
+df_reshaped = pd.read_csv('Coffee_Sales_Kaggle/index_1.csv')
 df_reshaped['datetime'] = pd.to_datetime(df_reshaped['datetime'])
 df_reshaped['year_month'] = df_reshaped['datetime'].dt.to_period('M').astype(str)
 df_reshaped['hour'] = df_reshaped['datetime'].dt.hour
@@ -22,9 +22,9 @@ df_reshaped['hour'] = df_reshaped['datetime'].dt.hour
 with st.sidebar:
     st.title('Coffee Shop Sales')
     
-    year_list = list(df_reshaped.year_month.unique())[::-1]
+    year_month_list = list(df_reshaped.year_month.unique())[::-1]
 
-    selected_year = st.selectbox('Select a year', year_list, index=len(year_list)-1)
+    selected_year = st.selectbox('Select a Date', year_month_list, index=len(year_month_list)-1)
     df_selected_year_month = df_reshaped[df_reshaped.year_month == selected_year]
     df_selected_year_month_sorted = df_selected_year_month.sort_values(by="money", ascending=False)
 
@@ -38,19 +38,38 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
     heatmap = (
         alt.Chart(input_df)
         .mark_rect()
-        .encode(y=alt.Y(f"{input_y}:O",axis=alt.Axis(title="Year",titleFontSize=18,titlePadding=15,titleFontWeight=900,labelAngle=0),),
+        .encode(y=alt.Y(f"{input_y}:O",axis=alt.Axis(title="Sales",titleFontSize=18,titlePadding=15,titleFontWeight=900,labelAngle=0),),
             x=alt.X(f"{input_x}:O",axis=alt.Axis(title="",titleFontSize=18,titlePadding=15,titleFontWeight=900),),
             color=alt.Color(f"sum({input_color}):Q",  legend=None,scale=alt.Scale(scheme=input_color_theme),),stroke=alt.value("black"),
             strokeWidth=alt.value(0.25),).properties(width=900).configure_axis(labelFontSize=12,titleFontSize=12,))
     return heatmap
 
 
+def make_line_chart(df, x, y, color=None, title="Line Chart"):
+    chart = alt.Chart(df).mark_line(point=True).encode(
+        x=alt.X(x, title=x),
+        y=alt.Y(y, title=y),
+        tooltip=[x, y] + ([color] if color else [])
+    )
+    # Styling
+    chart = chart.properties(
+        title=title,
+        width=900,
+        height=400
+    ).interactive()
+
+    return chart
+
 col = st.columns((1.5, 4.5, 2), gap='medium')
 
 
 
 with col[1]:
+
     st.markdown('#### Total Sales')
-    
+
     heatmap = make_heatmap(df_reshaped, 'hour', 'coffee_name', 'money', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
+
+    line_chart = make_line_chart(df_reshaped, 'year_month', 'money', selected_color_theme)
+    st.altair_chart(line_chart, use_container_width=True)
